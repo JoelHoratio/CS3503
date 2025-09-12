2965d690c1b0dd7d904211fcca54dac3ca192785
JoelHoratio <ramsayjoel7804@gmail.com>
2025-09-11 23:39:37 -0400

    Add files via upload

---
+++ convert.h
@@ -0,0 +1,23 @@
+//
+// Created by ramsa on 9/11/2025.
+//
+#ifndef CONVERT_H
+#define CONVERT_H
+
+#include <stdint.h>
+
+
+void div_convert(uint32_t n, int base, char *out);
+
+
+void sub_convert(uint32_t n, int base, char *out);
+
+
+void print_tables(uint32_t n);
+
+#endif /* CONVERT_H */
+
+#ifndef CS3503_CONVERT_H
+#define CS3503_CONVERT_H
+
+#endif //CS3503_CONVERT_H
